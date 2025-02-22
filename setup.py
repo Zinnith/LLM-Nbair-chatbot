32852803cd4f99e3979e4e3ec5a899c4cbb1a803
Zinnith-vmj <zinnithvmj@gmail.com>
2025-02-22 20:00:27 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'LLM-NBAIR-CHATBOT',
+    version= '0.0.0',
+    author= 'ZINNITH',
+    author_email= 'zinnithvmj@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
